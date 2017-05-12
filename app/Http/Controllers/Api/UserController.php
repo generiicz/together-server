ef7dab1897aa2a5b377408d9d9c5c5065d869204
Selmarinel <selmarinel@gmail.com>
2017-05-12 10:55:48 +0300

    TOG-3

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -80,7 +80,7 @@ class UserController extends Controller
             $user->saveCoverByFile($request->file('cover'));
         }
 
-        if($request->input('name')) {
+        if ($request->input('name')) {
             $user->name = $request->input('name');
         }
 
@@ -91,5 +91,69 @@ class UserController extends Controller
         return $this->sendJson([
             'user' => $user
         ]);
+    }
+
+    /**
+     * @SWG\Definition(
+     *            definition="UserToFind",
+     * 			@SWG\Property(property="name", type="string"),
+     *        )
+     */
+    /**
+     * @SWG\Definition(
+     *            definition="UserInfo",
+     * 			@SWG\Property(property="id", type="integer"),
+     * 			@SWG\Property(property="name", type="string"),
+     * 			@SWG\Property(property="cover", type="string"),
+     *        )
+     */
+    /**
+     * @SWG\Definition(
+     *            definition="UserList",
+     * 			@SWG\Property(property="list", type="array", items=@SWG\Schema(ref="#/definitions/UserInfo"),),
+     *        )
+     */
+
+    /**
+     * @SWG\Post(
+     *      path="/find",
+     *      operationId="findByName",
+     *      tags={"user"},
+     *      summary="Find User",
+     *      description="Find User by Name",
+     *      security={{"X-Api-Token":{}}},
+     *      @SWG\Parameter(
+     *          name="userName", in="body", required=true, description="User Post Data",
+     *          @SWG\Schema(ref="#/definitions/UserToFind"),
+     *      ),
+     *      @SWG\Response(
+     *          response=200,
+     *          @SWG\Schema(ref="#/definitions/UserList"),
+     *          description="successful operation"
+     *       ),
+     *       @SWG\Response(response=400, description="Bad request"),
+     *     )
+     *
+     */
+    public function findByNameAction(Request $request)
+    {
+        $validator = $this->getValidationFactory()->make($request->all(), [
+            'name' => 'required|string|max:25|min:3',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->validationError($validator->errors());
+        }
+        $findCollection = User::query()
+            ->where("name", "like", "%" . $request->get("name") . "%")
+            ->get();
+        $result = [];
+        foreach ($findCollection as $user) {
+            /**
+             * Todo: Process information by user
+             */
+            $result[] = $user;
+        }
+        return $this->sendJson(["list" => $result]);
     }
 }
